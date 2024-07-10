b0c4a052afac9e096753d46c875107714c188245
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-07-10 12:36:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Log4Net.AspNetCore;
+
+namespace Log4netLoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up Dependency Injection
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(configure => configure
+                    .AddLog4Net(@"C:\Source\Dotnet\logfournet\Log4netLoggingSample\log4net.config"))
+                .BuildServiceProvider();
+
+            // Get a logger
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            // Use the logger
+            logger.LogInformation("This is an information log.");
+            logger.LogWarning("This is a warning log.");
+            logger.LogError("This is an error log.");
+
+            Console.WriteLine("Logging complete. Check the console output and log file.");
+
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
