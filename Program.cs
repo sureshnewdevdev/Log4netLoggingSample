303c69858204472f52b58859e31998c1d748cc1f
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-07-30 12:19:36 +0530

    Logged based on Type

--- Program.cs
+++ Program.cs
@@ -18,10 +18,37 @@ namespace Log4netLoggingSample
             // Get a logger
             var logger = serviceProvider.GetService<ILogger<Program>>();
 
+            Console.WriteLine("Enter your Choice");
+            Console.WriteLine("Choices are 1-Read, 2-  write, 3 - exit");
+            string choice = Console.ReadLine();
+
+            while (choice != "3")
+            {
+                if (choice == "1")
+                {
+                    logger.LogInformation("Uer choice is One(read).");
+                    logger.LogError("Uer choice is One(read).");
+
+                }
+                if (choice == "2")
+                {
+                    logger.LogWarning("Uer choice is two(write).");
+
+                }
+                if(choice == "3")
+                {
+                    logger.LogCritical($"Uer choice is three. selected time is {DateTime.Now.ToString()}");
+                }
+                choice = Console.ReadLine();
+            }
+
+
+
+
             // Use the logger
-            logger.LogInformation("This is an information log.");
-            logger.LogWarning("This is a warning log.");
-            logger.LogError("This is an error log.");
+            //logger.LogInformation("This is an information log.");
+            //logger.LogWarning("This is a warning log.");
+            //logger.LogError("This is an error log.");
 
             Console.WriteLine("Logging complete. Check the console output and log file.");
 
